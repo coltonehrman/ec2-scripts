4cad693dcaf05a14d192de880626db1d599c4657
Colton Ehrman <coltonje95@gmail.com>
2024-01-07 11:05:34 -0600

    feat: update to redirect logs for app & kill 443

--- kill-existing-app.sh
+++ kill-existing-app.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
 fuser -k 80/tcp
+fuser -k 443/tcp
--- run-app.sh
+++ run-app.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 cd /root/app
-nohup npm run start > /dev/null 2>&1 &
+nohup npm run start > /var/log/app/out.log 2> /var/log/app/err.log &
